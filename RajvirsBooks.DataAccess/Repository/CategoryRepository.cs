f7f4c1df2cf3f08b7a9227d6efa6d605792383ef
Rajvir Chalthanwala <105453375+RajvirChalthanwala@users.noreply.github.com>
2022-11-12 18:35:00 -0500

    ICategoryRepository.cs modified

---
+++ RajvirsBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using RajvirsBooks.DataAccess.Repository.IRepository;
+using RajvirsBooks.Models;
+using RajvirsBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RajvirsBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ RajvirsBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RajvirsBooks.DataAccess.Repository.IRepository
+{
+    interface ICategoryRepository
+    {
+    }
+}
